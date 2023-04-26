f46103ba614dce503a7d36caed56c11e5e892729
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-04-26 22:45:27 +0300

    add migrations to create tables for user base

--- create_db.sh
+++ create_db.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-    DB_HOST=postgres
-    DB_PORT=5432
-    DB_PASS=postgres
-    DB_USER=root
-    DB_NAME=users
-    DB_SCHEMA=users_schema
+set -e
+set -u
 
-    psql -U postgres -c "CREATE DATABASE ${DB_NAME};"
-    psql -U postgres -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';"
-    psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
-    psql -d ${DB_NAME} -U ${DB_USER} -c "CREATE SCHEMA ${DB_SCHEMA};"
-    psql -d ${DB_NAME} -U ${DB_USER} -c "ALTER ROLE ${DB_USER} SET search_path = ${DB_SCHEMA};"
+echo "Creating operator database 'ku_users'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+CREATE DATABASE "ku_users";
+CREATE USER root WITH PASSWORD 'password' SUPERUSER;
+ALTER DATABASE ku_users OWNER TO root;
+\connect ku_users
+SET ROLE root;
+CREATE SCHEMA users_schema;
+ALTER ROLE root SET search_path TO users_schema,public;
 
+EOSQL
