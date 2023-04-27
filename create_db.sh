8b143707119fed0dd6f4cb40e6203a3164ad06b8
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-04-27 21:37:59 +0300

    fix comments

--- create_db.sh
+++ create_db.sh
@@ -6,11 +6,11 @@ set -u
 echo "Creating operator database 'ku_users'"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 CREATE DATABASE "ku_users";
-CREATE USER user_for_ku_users WITH PASSWORD 'password' SUPERUSER;
-ALTER DATABASE ku_users OWNER TO user_for_ku_users;
+CREATE USER ku_users_app WITH PASSWORD 'password' SUPERUSER;
+ALTER DATABASE ku_users OWNER TO ku_users_app;
 \connect ku_users
-SET ROLE user_for_ku_users;
+SET ROLE ku_users_app;
 CREATE SCHEMA users_schema;
-ALTER ROLE user_for_ku_users SET search_path TO users_schema,public;
+ALTER ROLE ku_users_app SET search_path TO users_schema,public;
 
 EOSQL
