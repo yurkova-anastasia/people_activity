442d04129737fb8073aaa5756ec4923fae7beb79
yurkova-anastasia <115931603+yurkova-anastasia@users.noreply.github.com>
2023-05-08 14:54:55 +0300

    add shell script (#1)
    Co-authored-by: AnastYurkova <anastasia.yurkova2003@mail.ru>

---
+++ create_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo "Creating operator database 'ku_users'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+CREATE DATABASE "ku_users";
+CREATE USER ku_users_app WITH PASSWORD 'password' SUPERUSER;
+ALTER DATABASE ku_users OWNER TO ku_users_app;
+\connect ku_users
+SET ROLE ku_users_app;
+CREATE SCHEMA users_schema;
+ALTER ROLE ku_users_app SET search_path TO users_schema,public;
+
+EOSQL
