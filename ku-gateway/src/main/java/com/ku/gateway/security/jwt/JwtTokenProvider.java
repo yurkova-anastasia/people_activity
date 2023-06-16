cff534b663da7462c0afe5d51af14a89ead12313
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-06-16 17:20:22 +0300

    fix comments

--- ku-gateway/src/main/java/com/ku/gateway/security/jwt/JwtTokenProvider.java
+++ ku-gateway/src/main/java/com/ku/gateway/security/jwt/JwtTokenProvider.java
@@ -9,11 +9,16 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.Clock;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Date;
 
 import static java.lang.String.format;
@@ -25,37 +30,44 @@ public class JwtTokenProvider {
     @Value("${spring.security.jwt.issuer}")
     private String issuer;
 
-
     private long expiration;
 
+    private Clock clock;
+
     @Value("${spring.security.jwt.expiration}")
-    public void setExpiration(String expiration, Duration duration) {
+    public void setExpiration(Duration duration) {
         this.expiration = duration.toMillis();
+    }
+
+    @Autowired
+    public void setClock(Clock clock) {
+        this.clock = clock;
     }
 
     private final Key secret = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     public String generateAccessToken(JwtUser user) {
+        LocalDateTime ldt = LocalDateTime.now(clock).plusSeconds(expiration);
         return "Bearer " + Jwts.builder()
                 .setSubject(format("%s,%s", user.getId(), user.getUsername()))
                 .setIssuer(issuer)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiration)) // 1 week
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .setExpiration(Date.from(ldt.atZone(ZoneId.ofOffset("UTC", ZoneOffset.UTC)).toInstant()))
+                .signWith(secret)
                 .compact();
     }
 
     public String getUsername(String token) {
-        Claims claims = Jwts.parser()
+        Claims claims = Jwts.parserBuilder()
                 .setSigningKey(secret)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
-
         return claims.getSubject().split(",")[1];
     }
 
     public boolean validate(String token) {
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token);
             return true;
         } catch (SignatureException ex) {
             log.error("Invalid JWT signature - {}", ex.getMessage());
