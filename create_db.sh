b0a7eadf23fb2b7522cc322116686b81d2279dfd
yurkova-anastasia <115931603+yurkova-anastasia@users.noreply.github.com>
2023-07-24 18:14:07 +0300

    creating database for devices (#4)
    Co-authored-by: AnastYurkova <anastasia.yurkova2003@mail.ru>

--- create_db.sh
+++ create_db.sh
@@ -13,3 +13,14 @@ CREATE SCHEMA users_schema;
 ALTER ROLE ku_users_app SET search_path TO users_schema,public;
 
 EOSQL
+
+echo "Creating operator database 'ku_devices'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+CREATE DATABASE "ku_devices";
+CREATE USER ku_devices_app WITH PASSWORD 'password' SUPERUSER;
+ALTER DATABASE ku_devices OWNER TO ku_devices_app;
+\connect ku_devices
+SET ROLE ku_devices_app;
+CREATE SCHEMA devices_schema;
+ALTER ROLE ku_devices_app SET search_path TO devices_schema,public;
+EOSQL
