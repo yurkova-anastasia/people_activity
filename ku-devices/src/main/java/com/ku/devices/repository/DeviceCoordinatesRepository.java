f52d4e3f8ce2c9616328d2eaae00e70d762a651a
Andrei <andrei.yurueu1@gmail.com>
2023-08-09 16:28:33 +0300

    batch saving of device coordinates

--- ku-devices/src/main/java/com/ku/devices/repository/DeviceCoordinatesRepository.java
+++ ku-devices/src/main/java/com/ku/devices/repository/DeviceCoordinatesRepository.java
@@ -1,7 +1,6 @@
 package com.ku.devices.repository;
 
 import com.ku.common.dto.DeviceCoordinatesDto;
-import com.ku.common.dto.DeviceCoordinatesSaveDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,66 +11,30 @@ import java.util.List;
 @Repository
 public class DeviceCoordinatesRepository {
 
-    public static final String FIND_BY_ID = "SELECT * FROM device_coordinates WHERE id = :id";
-    public static final String FIND_ALL_BY_DEVICE_ID = """
-          SELECT * FROM device_coordinates
-          WHERE device_id = :deviceId
-          LIMIT :limit OFFSET :offset
-        """;
-
     public static final String SAVE = """
-          INSERT INTO device_coordinates (latitude, longitude, active, deviceId)
-          VALUES (:latitude, :longitude, :active, :deviceId)
-        """;
-
-    public static final String UPDATING_AFTER_SAVE = """
-          UPDATE device_coordinates
-          SET active = false
-          WHERE device_id = :deviceId
-        """;
+              INSERT INTO device_coordinates (latitude, longitude, active, deviceId)
+              VALUES (:latitude, :longitude, :active, :deviceId)
+            """;
 
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    @Autowired
-    public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
-
-    public DeviceCoordinatesDto findById(Long id) {
-        return namedParameterJdbcTemplate.queryForObject(
-                FIND_BY_ID,
-                new MapSqlParameterSource().addValue("id", id),
-                DeviceCoordinatesDto.class
+    public void save(List<DeviceCoordinatesDto> coordinatesSaveDtos) {
+        coordinatesSaveDtos.forEach(saveDto -> namedParameterJdbcTemplate.update(
+                SAVE,
+                fillParametersForSaveQuery(saveDto))
         );
     }
 
-    public List<DeviceCoordinatesDto> findAllByDeviceId(Long deviceId, Long limit, Long offset) {
-        return namedParameterJdbcTemplate.queryForList(
-                FIND_ALL_BY_DEVICE_ID,
-                fillParametersForFindByDeviceIdQuery(deviceId, limit, offset),
-                DeviceCoordinatesDto.class
-        );
-    }
-
-    private MapSqlParameterSource fillParametersForFindByDeviceIdQuery(Long deviceId, Long limit, Long offset) {
-        return new MapSqlParameterSource()
-                .addValue("deviceId", deviceId)
-                .addValue("limit", limit)
-                .addValue("offset", offset);
-    }
-
-    public void save(DeviceCoordinatesSaveDto coordinatesSaveDto) {
-        namedParameterJdbcTemplate.update(SAVE, fillParametersForSaveQuery(coordinatesSaveDto));
-        namedParameterJdbcTemplate.update(
-                UPDATING_AFTER_SAVE,
-                new MapSqlParameterSource().addValue("deviceId", coordinatesSaveDto.getDeviceId()));
-    }
-
-    private MapSqlParameterSource fillParametersForSaveQuery(DeviceCoordinatesSaveDto coordinatesSaveDto) {
+    private MapSqlParameterSource fillParametersForSaveQuery(DeviceCoordinatesDto coordinatesSaveDto) {
         return new MapSqlParameterSource()
                 .addValue("latitude", coordinatesSaveDto.getLatitude())
                 .addValue("longitude", coordinatesSaveDto.getLongitude())
                 .addValue("active", true)
                 .addValue("deviceId", coordinatesSaveDto.getDeviceId());
+    }
+
+    @Autowired
+    public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 }
--- ku-devices/src/main/java/com/ku/devices/service/DeviceCoordinatesService.java
+++ ku-devices/src/main/java/com/ku/devices/service/DeviceCoordinatesService.java
@@ -1,10 +1,12 @@
 package com.ku.devices.service;
 
-import com.ku.common.dto.DeviceCoordinatesSaveDto;
+import com.ku.common.dto.DeviceCoordinatesDto;
 import com.ku.devices.repository.DeviceCoordinatesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,14 +14,13 @@ public class DeviceCoordinatesService {
 
     private DeviceCoordinatesRepository coordinatesRepository;
 
+    public void saveDeviceCoordinates(List<DeviceCoordinatesDto> coordinatesSaveDtos) {
+        coordinatesRepository.save(coordinatesSaveDtos);
+        log.info("Device coordinates persisted {}", coordinatesSaveDtos);
+    }
+
     @Autowired
     public void setDeviceCoordinatesRepository(DeviceCoordinatesRepository coordinatesRepository) {
         this.coordinatesRepository = coordinatesRepository;
-    }
-
-
-    public void saveDeviceCoordinates(DeviceCoordinatesSaveDto coordinatesSaveDto) {
-        coordinatesRepository.save(coordinatesSaveDto);
-        log.info("Device coordinates persisted {}", coordinatesSaveDto);
     }
 }
