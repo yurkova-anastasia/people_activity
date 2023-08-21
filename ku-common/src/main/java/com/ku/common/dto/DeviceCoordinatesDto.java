5709c60f6a254f28a7f409e7adfeb7d9bfabc282
Andrei <andrei.yurueu1@gmail.com>
2023-08-21 15:42:23 +0300

    refactoring dto

--- ku-common/src/main/java/com/ku/common/dto/DeviceCoordinatesDto.java
+++ ku-common/src/main/java/com/ku/common/dto/DeviceCoordinatesDto.java
@@ -1,10 +1,8 @@
 package com.ku.common.dto;
 
 import lombok.Data;
-import lombok.experimental.Accessors;
 
 @Data
-@Accessors(chain = true)
 public class DeviceCoordinatesDto {
 
     private double latitude;
