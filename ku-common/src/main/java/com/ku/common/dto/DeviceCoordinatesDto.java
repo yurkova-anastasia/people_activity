d95db2b5eed7135ebef1bdf51859604af5d70e89
Andrei <andrei.yurueu1@gmail.com>
2023-08-22 11:18:07 +0300

    @Accessors in DTO

--- ku-common/src/main/java/com/ku/common/dto/DeviceCoordinatesDto.java
+++ ku-common/src/main/java/com/ku/common/dto/DeviceCoordinatesDto.java
@@ -1,8 +1,10 @@
 package com.ku.common.dto;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class DeviceCoordinatesDto {
 
     private double latitude;
