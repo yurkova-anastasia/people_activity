4b14cc63524c0bf4995dba7da92e329340ee6a6c
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-09-17 22:12:44 +0300

    add redis caching layer

--- ku-gateway/src/main/java/com/ku/gateway/controller/DevicePingController.java
+++ ku-gateway/src/main/java/com/ku/gateway/controller/DevicePingController.java
@@ -2,7 +2,6 @@ package com.ku.gateway.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ku.common.dto.DevicePingDto;
-import com.ku.gateway.util.DevicePingProcessor;
 import com.ku.gateway.service.DevicePingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/devicePing")
+@RequestMapping("/devicePings")
 public class DevicePingController {
 
     private DevicePingService devicePingService;
 
-    private DevicePingProcessor devicePingProcessor;
-
     @PostMapping
-    public void save(@RequestBody DevicePingDto ping) throws JsonProcessingException {
-        devicePingService.save(ping);
-    }
-
-    @PostMapping("/cache")
-    public ResponseEntity<String> receiveDevicePing(@RequestBody DevicePingDto devicePingDto)
+    public ResponseEntity<String> save(@RequestBody DevicePingDto devicePingDto)
             throws JsonProcessingException {
-        devicePingProcessor.processDevicePing(devicePingDto);
+        devicePingService.save(devicePingDto);
         return ResponseEntity.ok("Device ping processed successfully");
     }
 
     @Autowired
     public void setDevicePingsService(DevicePingService devicePingsService) {
         this.devicePingService = devicePingsService;
-    }
-
-    @Autowired
-    public void setDevicePingProcessor(DevicePingProcessor devicePingProcessor) {
-        this.devicePingProcessor = devicePingProcessor;
     }
 }
--- ku-gateway/src/main/java/com/ku/gateway/service/DevicePingService.java
+++ ku-gateway/src/main/java/com/ku/gateway/service/DevicePingService.java
@@ -1,18 +1,134 @@
 package com.ku.gateway.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ku.common.dto.DevicePingDto;
 import com.ku.gateway.service.producer.DevicePingProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DevicePingService {
+    @Value("${normalRanges.weight.min}")
+    private float minWeight;
+
+    @Value("${normalRanges.weight.max}")
+    private float maxWeight;
+
+    @Value("${normalRanges.height.min}")
+    private float minHeight;
+
+    @Value("${normalRanges.height.max}")
+    private float maxHeight;
+
+    @Value("${normalRanges.pulse.min}")
+    private int minPulse;
+
+    @Value("${normalRanges.pulse.max}")
+    private int maxPulse;
+
+    @Value("${normalRanges.temperature.min}")
+    private float minTemperature;
+
+    @Value("${normalRanges.temperature.max}")
+    private float maxTemperature;
+
+    @Value("${normalRanges.heartbeat.min}")
+    private int minHeartbeat;
+
+    @Value("${normalRanges.heartbeat.max}")
+    private int maxHeartbeat;
+
+    @Value("${normalRanges.respiratoryRate.min}")
+    private int minRespiratoryRate;
+
+    @Value("${normalRanges.respiratoryRate.max}")
+    private int maxRespiratoryRate;
+
+    @Value("${normalRanges.systolicPressure.min}")
+    private int minSystolicPressure;
+
+    @Value("${normalRanges.systolicPressure.max}")
+    private int maxSystolicPressure;
+
+    @Value("${normalRanges.diastolicPressure.min}")
+    private int minDiastolicPressure;
+
+    @Value("${normalRanges.diastolicPressure.max}")
+    private int maxDiastolicPressure;
+
+    private ObjectMapper objectMapper;
+
+    private RedisTemplate<String, String> redisTemplate;
 
     private DevicePingProducer producer;
 
-    public void save(DevicePingDto ping) throws JsonProcessingException {
-        producer.sendMessage(ping);
+    public void save(DevicePingDto devicePingDto) throws JsonProcessingException {
+        if (hasDeviation(devicePingDto)) {
+            //проверяем отклонения
+            //если есть, то скидываешь в тот топик, который сохраняет данные
+            producer.sendMessage(devicePingDto);
+        } else {
+            //если отклонений нет, обновляешь редис
+            String devicePing = objectMapper.writeValueAsString(devicePingDto);
+            redisTemplate.opsForList().rightPush(String.valueOf(devicePingDto.getDeviceId()), devicePing);
+        }
+    }
+
+    private boolean hasDeviation(DevicePingDto devicePingDto) {
+        if (devicePingDto.getWeight() < minWeight
+                || devicePingDto.getWeight() > maxWeight) {
+            return true;
+        }
+
+        if (devicePingDto.getHeight() < minHeight
+                || devicePingDto.getHeight() > maxHeight) {
+            return true;
+        }
+
+        if (devicePingDto.getPulse() < minPulse
+                || devicePingDto.getPulse() > maxPulse) {
+            return true;
+        }
+
+        if (devicePingDto.getTemperature() < minTemperature
+                || devicePingDto.getTemperature() > maxTemperature) {
+            return true;
+        }
+
+        if (devicePingDto.getHeartbeat() < minHeartbeat
+                || devicePingDto.getHeartbeat() > maxHeartbeat) {
+            return true;
+        }
+
+        if (devicePingDto.getRespiratoryRate() < minRespiratoryRate
+                || devicePingDto.getRespiratoryRate() > maxRespiratoryRate) {
+            return true;
+        }
+
+        if (devicePingDto.getSystolicPressure() < minSystolicPressure
+                || devicePingDto.getSystolicPressure() > maxSystolicPressure) {
+            return true;
+        }
+
+        if (devicePingDto.getDiastolicPressure() < minDiastolicPressure
+                || devicePingDto.getDiastolicPressure() > maxDiastolicPressure) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Autowired
+    public void setRedisTemplate(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
 
     @Autowired
